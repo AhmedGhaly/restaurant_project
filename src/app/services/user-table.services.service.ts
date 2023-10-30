551473803ad2e03833fc2e3a7e5ef158855608c6
AhmedEisa <AhmedEisa@DESKTOP-O9V8KLJ>
2023-10-30 18:54:46 +0200

    eee

--- src/app/services/user-table.services.service.ts
+++ src/app/services/user-table.services.service.ts
@@ -2,9 +2,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.dev';
+import { getCookie } from 'typescript-cookie';
 
-const token =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiZWlzYSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL25hbWVpZGVudGlmaWVyIjoiOTk3NzFjOTUtN2NlYi00YTA1LWI1YWUtMzg5MmVmZWYxNTY5IiwianRpIjoiMmUwODhiMjEtNmYwNS00Y2FiLWI3NTMtY2EzNmUwNTAzZTA1IiwiZXhwIjoxNjk4NTQ4NDA5LCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo3MDU4LyIsImF1ZCI6Imh0dHBzOi8vbG9jYWxob3N0OjQyMDAvIn0.J8bhX872-WA8d2O9E317mFbxGTMLu4IZJrZGSVE9sWI';
+const JsonToken = getCookie('User');
+const token = JsonToken != undefined ? JSON.parse(JsonToken) : null;
 
 const headers = new HttpHeaders({
   Authorization: `Bearer ${token}`,
